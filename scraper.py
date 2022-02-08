c49f2c332a49e6e33201b0ca90f099bb7a2d5cfe
Seagleb <seagleb@gmail.com>
2022-02-07 19:30:27 -0600

    Updated years

--- scraper.py
+++ scraper.py
@@ -5,47 +5,58 @@ from IPython.core.display import clear_output
 from warnings import warn
 import requests
 
+
 def scrapeMovies():
 
-    years = [str(i) for i in range(1980, 2019)]
+    years = [str(i) for i in range(1985, 2021)]
     headers = {"Accept-Language": "en-US, en;q=0.5"}
     start_time = time()
     request_int = 0
     movie_list = []
 
-    for year in years:  
-        response = requests.get('http://www.imdb.com/search/title?release_date='
-                                + year
-                                + '&sort=num_votes,desc&page=1', headers=headers)
+    for year in years:
+        response = requests.get(
+            "http://www.imdb.com/search/title?release_date="
+            + year
+            + "&sort=num_votes,desc&page=1",
+            headers=headers,
+        )
         elapsed_time = time() - start_time
-        sleep(randint(2,5))
+        sleep(1)
+        ##sleep(randint(2, 5))
         request_int += 1
 
-        print('Request: {}; Year: {}; Frequency: {} requests/s'.format(request_int, year,
-                                                            request_int/elapsed_time))
+        print(
+            "Request: {}; Year: {}; Frequency: {} requests/s".format(
+                request_int, year, request_int / elapsed_time
+            )
+        )
         clear_output(wait=True)
 
         if response.status_code != 200:
-            warn('Request: {}; Status code: {}'.format(
-                request_int, response.status_code))
+            warn(
+                "Request: {}; Status code: {}".format(request_int, response.status_code)
+            )
         if request_int > 72:
-            warn('Number of requests was greater than expected.')
+            warn("Number of requests was greater than expected.")
             break
 
-        page_html = BeautifulSoup(response.text, 'html.parser')
-        movie_containers = page_html.find_all('div', class_='lister-item mode-advanced')
+        page_html = BeautifulSoup(response.text, "html.parser")
+        movie_containers = page_html.find_all("div", class_="lister-item mode-advanced")
 
         for movie in movie_containers:
             movie_name = movie.h3.a.text
             movie_list.append(movie_name)
-            
+
     return movie_list
+
 
 def writeMovies(movie_list):
     for name in movie_list:
-        with open('movies.txt', 'a') as output:
+        with open("movies.txt", "a") as output:
             output.write(name)
-            output.write('\n')
+            output.write("\n")
+
 
 if __name__ == "__main__":
     movie_list = scrapeMovies()
